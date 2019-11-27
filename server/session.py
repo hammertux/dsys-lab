f2fa58b7a9e41e338b971ddad405925b094f219d
Max Meijer <mmeijer@eljakim.nl>
2019-11-27 22:30:01 +0100

    Makes sure server actually sends messages through

--- server/session.py
+++ server/session.py
@@ -3,6 +3,7 @@ import uuid # Contains code for universally unique identifiers
 import queue
 import threading
 from . import time_utils
+from . import message as message_mod
 
 class SessionStore:
   def __init__(self):
@@ -18,34 +19,72 @@ class SessionStore:
 
 class Session:
   def __init__(self):
+    # uuid of the session
     self.uuid = uuid.uuid4()
+    # queue containing messages that need to be sent to the clients
     self.messageQueue = queue.Queue()
+    # consistency metrics
     self.numUpdatesSent = 0
     self.numMessagesSent = 0
     self.numUpdatesAcknowledged = 0
     self.numMessagesAcknowledged = 0
+
+    # expiration time of the session
     self.expirationTime = time_utils.current_server_time()
-    self.sessionLength = 10*1000*1000 # session length in microseconds
+
+    # length of a session in microseconds
+    self.sessionLength = 10*1000*1000 # 10 seconds
+
+    # lock used by the Acknowledge function to avoid concurrency issues
     self.__acknowledgementLock = threading.Lock()
+
+    # indicates to this thread whether it should stop
+    self.__should_shutdown = False
+
+    # lock used by ReceiveUpdates
+    self.__receiveUpdatesLock = threading.Lock()
   
   def Acknowledge(self, acknowledgement):
     # First check if locking is necessary
     if acknowledgement.numUpdatesReceived > self.numUpdatesAcknowledged or acknowledgement.numMessagesReceived > self.numMessagesAcknowledged:
-      self.__acknowledgementLock.acquire()
-      # take the maximum number of updates acknowledged because the acknowledgements are not guaranteed to arrive in the correct order
-      self.numUpdatesAcknowledged = max(acknowledgement.numUpdatesReceived, self.numUpdatesAcknowledged)
-      self.numMessagesAcknowledged = max(acknowledgement.numMessagesReceived, self.numMessagesAcknowledged)
-      self.__acknowledgementLock.release()
+      # acquire the lock to avoid concurrency issues
+      with self.__acknowledgementLock:
+        # take the maximum number of updates acknowledged because the acknowledgements are not guaranteed to arrive in the correct order
+        self.numUpdatesAcknowledged = max(acknowledgement.numUpdatesReceived, self.numUpdatesAcknowledged)
+        self.numMessagesAcknowledged = max(acknowledgement.numMessagesReceived, self.numMessagesAcknowledged)
+  
+  # Triggers a shutdown of the active thread in this session
+  def trigger_shutdown(self):
+    self.__should_shutdown = True
+    try:
+      self.messageQueue.put_nowait(None)
+    except:
+      # queue is apparently full so no need to add new elements
+      pass
+
 
   def ReceiveUpdates(self):
+    # acquire the lock to ensure that only one such connection exists per session
+    # the lock is never released
+    if not self.__receiveUpdatesLock.acquire(False):
+      return
+    
+    # for every message in the queue
     while True:
       message = self.messageQueue.get() # blocks until there are elements in the queue
-      update = chat_pb2.Update()
-      update.message = message
-      update.currentTime.timestamp = time_utils.current_server_time()
-      update.expirationTime.timestamp = self.expirationTime = update.currentTime + self.sessionLength
-      
-      yield update
+
+      # A None in the queue indicates to this thread that it should stop
+      if self.__should_shutdown:
+        break
+        
+      # construct an update message
+      update = message_mod.Update(self.sessionLength)
+      update.add_message(message)
+      print("Sending")
+
+      # send it to the client
+      yield update.to_chat_pb2()
+
 
   def is_session_expired(self):
-    return time_utils.current_server_time() > self.expirationTime4
+    return time_utils.current_server_time() > self.expirationTime
--- tests/test_chat_server.py
+++ tests/test_chat_server.py
@@ -1,54 +1,36 @@
 from server import chat_server as chat_server_mod
-from proto import chat_pb2_grpc, chat_pb2
-import grpc
+from proto import chat_pb2
 import unittest
-import threading
 import time
+from pprint import pprint
 
 class TestBasicServerFunctionality(unittest.TestCase):
   chat_server = None
 
-
   def setUp(self):
-    self.chat_server = chat_pb2_grpc.ChatServerStub(TestBasicServerFunctionality.chat_channel)
-
-
-  @classmethod
-  def setUpClass(cls):
-    super(TestBasicServerFunctionality, cls).setUpClass()
-    chat_server_mod.serve()
-    cls.chat_channel = grpc.insecure_channel('localhost:50051')
-
-
-  @classmethod
-  def tearDownClass(cls):
-    cls.chat_channel.close()
-    chat_server_mod.stop_serving()
-
+    self.servicer = chat_server_mod.ChatServicer()
 
   def test_server_gives_out_different_session_ids(self):
     default_thread = chat_pb2.Thread()
-    connectionResponse1 = self.chat_server.Connect(default_thread)
-    connectionResponse2 = self.chat_server.Connect(default_thread)
+    connectionResponse1 = self.servicer.Connect(default_thread, None)
+    connectionResponse2 = self.servicer.Connect(default_thread, None)
     self.assertNotEqual(connectionResponse1.session.uuid.hex, connectionResponse2.session.uuid.hex)
     self.assertNotEqual("00000000000000000000000000000000", connectionResponse1.session.uuid.hex)
+    self.assertEqual("00000000000000000000000000000000", connectionResponse1.session.thread.uuid.hex)
+    self.assertEqual("00000000000000000000000000000000", connectionResponse2.session.thread.uuid.hex)
 
   def test_receive_own_messages(self):
     default_thread = chat_pb2.Thread()
-    connectionResponse = self.chat_server.Connect(default_thread)
-    connectionResponse = self.chat_server.Connect(default_thread)
-    receive_response = self.chat_server.ReceiveUpdates(connectionResponse.session)
+    connectionResponse = self.servicer.Connect(default_thread, None)
+    connectionResponse = self.servicer.Connect(default_thread, None)
+    receive_response = self.servicer.ReceiveUpdates(connectionResponse.session, None)
     sentMessage = chat_pb2.SentMessage()
     sentMessage.contents = "Test123"
     sentMessage.timestamp.timestamp = int(round(time.time() * 1000 * 1000))
     sentMessage.acknowledgement.numMessagesSent = 1
     sentMessage.acknowledgement.session.uuid.hex = connectionResponse.session.uuid.hex
     sentMessage.acknowledgement.session.thread.uuid.hex = connectionResponse.session.thread.uuid.hex
-    self.chat_server.SendMessage(sentMessage)
-    self.assertIsNotNone(receive_response.next())
-    receive_response.close()
-
-
-
-if __name__ == '__main__':
-  unittest.main()
+    message_status = self.servicer.SendMessage(sentMessage, None)
+    self.assertEqual(0, message_status.statusCode)
+    received_update = next(receive_response)
+    self.assertEqual(sentMessage.contents, received_update.message[0].contents)
