34c036fab97c77b8356f939e3f2cd3e5780e3060
Mathijs Maijer <matevy1@gmail.com>
2019-11-28 06:15:46 +0100

    Rework display and reply system, refine session creation

--- client/chat_client.py
+++ client/chat_client.py
@@ -4,6 +4,7 @@ import sys
 import threading
 
 from time import gmtime, strftime
+import datetime
 
 import grpc
 from proto import chat_pb2
@@ -16,6 +17,7 @@ class Client:
         self.connection = chat_pb2_grpc.ChatServerStub(self.channel)
         self.default_thread = chat_pb2.Thread()
         self.current_thread = None
+        self.global_uuid = None
 
         # Key: session uuid hex
         # Value: dict(session, messages_sent, messages_received updates_received)
@@ -35,18 +37,19 @@ class Client:
 
         self.sessions[res.session.uuid.hex] = {'session': res.session, 'messages_sent': 0, 'messages_received': 0, 'updates_received': 0}
         self.current_thread = res.session.thread.uuid.hex
-        print('Gonna listen')
+        if thread == self.default_thread:
+            self.global_uuid = self.current_thread
+        print('Listening to new thread')
         self.start_listening(res.session)
 
     def start_listening(self, session):
         threading.Thread(target=self._listen, args=(session,), daemon=True).start()
-        
 
     def _listen(self, session):
         # Get server updates
         for update in self.connection.ReceiveUpdates(session):
             # Handle update
-            self.handle_update(update)
+            self.handle_update(session, update)
             # Acknowledge
             acknowledgement = chat_pb2.Acknowledgement(
                 session = session,
@@ -56,28 +59,45 @@ class Client:
             )
             timestamp = self.connection.Acknowlegde(acknowledgement)
 
-    def handle_update(self, update):
-        message = update.message
-        self.display(message)
+    def handle_update(self, session, update):
+        self.sessions[session.uuid.hex]['updates_received'] += 1
+
+        for msg in update.message:
+            self.sessions[session.uuid.hex]['messages_received'] += 1
+            self.display(msg)
 
     def display(self, msg):
-        print(msg.serverTime + ' | ' + msg.thread + ' | ' + msg.sender + ': ' + msg.contents + "\n")
+        time = str(msg.serverTime.timestamp)
+        formatted_time = datetime.datetime.fromtimestamp(int(time) / 1000 / 1000).strftime('%c')
+        thread = str(msg.thread.uuid.hex)
+        id = str(msg.uuid.hex)
+        suffix = ''
+        if thread == '00000000000000000000000000000000':
+            thread = 'global'
+            suffix = ' | ' + id
+
+        print(formatted_time + ' | ' + thread + ' | ' + ' unknown ' + ': ' + msg.contents + suffix + "\n")
 
     def handle_input(self, user_input):
-        time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        # print(time + ' | ' + self.username + ': ' + user_input + "\n")
-
         # Get thread
+        msg = user_input
         in_split = user_input.split(' ')
-        if len(in_split) > 1 and user_input.split(' ')[-2] == '|':
-            self.current_thread = user_input.split(' ')[-1]
+        if len(in_split) > 1 and in_split[-2] == '|':
+            self.current_thread = in_split[-1]
+            if self.current_thread == 'global':
+                self.current_thread = self.global_uuid
 
-            # Listen to new thread
-            thread = chat_pb2.Thread(uuid = self.current_thread)
-            self.connect(thread)
+            msg = ' '.join(in_split[:-2])
+
+            # Listen to new thread if no session has been created
+            if not self.thread_to_session(self.current_thread):
+                uuid = chat_pb2.UUID(hex=str(self.current_thread))
+                thread = chat_pb2.Thread(uuid = uuid)
+                self.connect(thread)
 
         # Send message to server
         s = self.sessions[self.thread_to_session(self.current_thread)]
+        s['messages_sent'] += 1
         acknowledgement = chat_pb2.Acknowledgement(
             session = s['session'],
             numUpdatesReceived = s['updates_received'],
@@ -86,7 +106,7 @@ class Client:
         )
         msg_obj = chat_pb2.SentMessage(
             acknowledgement=acknowledgement,
-            contents=user_input,
+            contents=msg,
             timestamp=chat_pb2.ServerTime(timestamp=self.current_time())
         )
         status = self.connection.SendMessage(msg_obj)
@@ -100,6 +120,7 @@ class Client:
         for k, v in self.sessions.items():
             if v['session'].thread.uuid.hex == thread_uuid:
                 return k
+        return None
 
 def run(client):
     while True:
